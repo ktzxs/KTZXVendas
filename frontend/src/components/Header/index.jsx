a882840b467218e060aa68d12f99ad209040c2e9
ktzxs <lucasalvesdacosta070@gmail.com>
2025-09-29 17:43:55 -0300

    confirmação

---
+++ frontend/src/components/Header/index.jsx
@@ -0,0 +1,27 @@
+import './Header.css'
+import { Link } from 'react-router-dom'
+
+export default function Header() {
+    return (
+        <header className='header'>
+            {/* Logo*/}
+            <div className='logo'>
+                <Link to='/'>
+                    <span className='logo-1'>K</span>
+                    <span className='logo-2'>t</span>
+                    <span className='logo-3'>z</span>
+                </Link>
+            </div>
+
+            {/* Busca */}
+            <div className='search-bar'>
+                <input type='text' placeholder='Buscar "Apartamento"' />
+                <div className='location'>
+                    <button className='search-btn'>
+                        <ion-icon name="search"></ion-icon>
+                    </button>
+                </div>
+            </div>
+        </header>
+    )
+}
